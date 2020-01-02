272fff9e544c53a63f40ce1b195bdcbb82a60324
Pipapa <yezhengmaolove@gmail.com>
2020-01-02 14:55:14 +0800

    [fector]c++11重写选择排序

--- 排序算法/InsertSort.h
+++ 排序算法/InsertSort.h
@@ -5,7 +5,7 @@
 
 template<class It, class Compare = std::less<value_type_t<It>>>
 void InsertSort(It begin, It end, Compare cmp = Compare()) {
-    for(auto it = begin; it != end; ++it) {
+    for(auto it = begin; it != end; it = std::next(it)) {
         auto const insertion = std::upper_bound(begin, it, *it, cmp);
         std::rotate(insertion, it, std::next(it));
     }
---
+++ 排序算法/SelectionSort.h
@@ -0,0 +1,13 @@
+#ifndef SELECTSORT_H_
+#define SELECTSORT_H_
+
+#include <Sort.h>
+
+template<class It, class Compare = std::less<value_type_t<It>>>
+void SelectionSort(It begin, It end, Compare cmp = Compare()) {
+    for(auto it = begin; it != end; it = std::next(it)) {
+        std::iter_swap(it, std::min_element(it, end));
+    }
+}
+
+#endif
