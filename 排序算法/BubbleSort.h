298972cb02d31e7372f90753e12062a88d5d1533
Pipapa <yezhengmaolove@gmail.com>
2020-01-02 15:29:21 +0800

    [refactor]c++11冒泡排序

--- 排序算法/BubbleSort.h
+++ 排序算法/BubbleSort.h
@@ -1,37 +1,22 @@
 #ifndef BUBBLESORT_H_
 #define BUBBLESORT_H_
-#include <vector>
-#include <algorithm>
-/*
- * File:            BubbleSort.h
- * Description:     冒泡排序
- * Author:          Pipapa
- * Email:           yezhengmaolove@gmail.com
- * Date:            2019-08-29 09:27:49
- * License:         GPL
- * */
 
-/*
- * Function:        BubbleSort
- * Description:     冒泡排序
- * Input:     
- *     arr          待排序容器
- *     l            容器左端点，闭区间
- *     r            容器右端点，闭区间
- * Return:    
- *     None
- * Note:      
- *     从r位置的元素开始，将最小的元素交换到l位置
- *     之后l++，即可保证arr[l..l`)元素为有序元素
- * Sample:    
- *     BubbleSort(arr, 0, arr.size() - 1);
- * */
-template<typename T>
-void BubbleSort(std::vector<T> &arr, int l, int r) {
-    for(int i = l; i < r; ++i) {
-        for(int j = r; j > i; --j) {
-            if(arr[j] < arr[j-1]) std::swap(arr[j], arr[j-1]);
+#include <Sort.h>
+
+template<class It, class Compare = std::less<value_type_t<It>>>
+void BubbleSort(It begin, It end, Compare cmp = Compare()) {
+    for(auto it = end, sorted = false; it != begin && !sorted;) {
+        sorted = true;
+        auto nit = std::prev(it);
+        for(auto fwdit = std::next(begin); fwdit != it; fwdit = std::next(fwdit)) {
+            if(cmp(*fwdit, *std::prev(fwdit))) {
+                std::iter_swap(fwdit, std::prev(fwdit));
+                sorted = false;
+                nit = fwdit;
+            }
         }
+        it = nit;
     }
 }
+
 #endif
