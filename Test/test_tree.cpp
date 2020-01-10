ee459fd6c2e82ccb01b5fb6bdc40fd33ed52e7a7
Pipapa <yezhengmaolove@gmail.com>
2020-01-10 13:15:40 +0800

    [test]init tree test unit

---
+++ Test/test_tree.cpp
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+#include "Tree.h"
+
+class TreeTest : public ::testing::Test {
+protected:
+    virtual void SetUp() {}
+    virtual void TearDown() {}
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+};
+
+void TreeTest::SetUpTestCase() {
+
+}
+
+void TreeTest::TearDownTestCase() {
+
+}
+
+TEST_F(TreeTest, Node) {
+    tree_node_t<int> node;
+}
