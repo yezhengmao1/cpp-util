be27845dfebd4421b78fc2a5e68ba90624fe88dc
Pipapa <yezhengmaolove@gmail.com>
2020-01-11 13:12:44 +0800

    [fix]编译器行为

--- Sort/BubbleSort.h
+++ Sort/BubbleSort.h
@@ -5,7 +5,8 @@
 
 template<class It, class Compare = std::less<value_type_t<It>>>
 void BubbleSort(It begin, It end, Compare cmp = Compare()) {
-    for(auto it = end, sorted = false; it != begin && !sorted;) {
+    bool sorted = false;
+    for(auto it = end; it != begin && !sorted;) {
         sorted = true;
         auto nit = std::prev(it);
         for(auto fwdit = std::next(begin); fwdit != it; fwdit = std::next(fwdit)) {
