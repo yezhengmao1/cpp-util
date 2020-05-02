53e580b03645fb619ee727b5fb2f9214c1f8ce13
Pipapa <yezhengmaolove@gmail.com>
2020-05-02 14:35:23 +0800

    [feat] add concat integer_sequence template

--- Math/Prime.h
+++ Math/Prime.h
@@ -1,35 +1,77 @@
 #ifndef PRIME_H_
 #define PRIME_H_
 
+#include <csignal>
 #include <utility>
 
-template<unsigned i, unsigned head, unsigned ...tail>
-struct IsPrime {
-  using value = typename std::conditional<i % head != 0,
-                                 typename IsPrime<i, tail...>::value,
-                                 std::false_type>::type;
+template<unsigned i, typename T>
+struct IsPrime { };
+
+template<unsigned i, unsigned head, unsigned ...args>
+struct IsPrime<i, std::integer_sequence<unsigned, head, args...>> {
+  using value = typename std::conditional<
+      i % head == 0,
+      std::false_type,
+      typename IsPrime<i, std::integer_sequence<unsigned, args...>>::value
+  >::type;
 };
 
 template<unsigned i, unsigned head>
-struct IsPrime<i, head> {
-  using value = typename std::conditional<i % head != 0,
-                                std::true_type,
-                                std::false_type>::type;
+struct IsPrime<i, std::integer_sequence<unsigned, head>> {
+  using value = typename std::conditional<
+      i % head == 0,
+      std::false_type,
+      std::true_type
+    >::type;
 };
 
-template<unsigned i, typename T>
-struct AppendPrime;
+template<typename T, typename U>
+struct Concat;
 
-template<unsigned i, unsigned ...args>
-struct AppendPrime<i, std::integer_sequence<unsigned, args...>> {
-  using value = typename std::conditional<IsPrime<i, args...>::value::value,
-                           std::integer_sequence<unsigned, i, args...>,
-                           std::integer_sequence<unsigned, args...>>::type;
+template<unsigned ...l, unsigned ...r>
+struct Concat<std::integer_sequence<unsigned, l...>, std::integer_sequence<unsigned, r...>> {
+  using value = typename std::integer_sequence<unsigned, l..., r...>;
+};
+
+template<unsigned N, typename T>
+struct ConcatPrime { };
+
+template<unsigned N, unsigned ...args>
+struct ConcatPrime<N, std::integer_sequence<unsigned, args...>> {
+  using value = typename std::conditional<
+      IsPrime<N, std::integer_sequence<unsigned, args...>>::value::value,
+      std::integer_sequence<unsigned, N, args...>,
+      std::integer_sequence<unsigned, args...>
+    >::type;
+};
+
+template<unsigned L, unsigned R, typename T>
+struct Table { };
+
+template<unsigned L, unsigned R, unsigned ...args>
+struct Table<L, R, std::integer_sequence<unsigned, args...>> {
+  using value = typename Concat<
+    typename std::conditional<
+        IsPrime<R, std::integer_sequence<unsigned, args...>>::value::value,
+        std::integer_sequence<unsigned, R>,
+        std::integer_sequence<unsigned>
+      >::type,
+    typename Table<L, R-1, std::integer_sequence<unsigned, args...>>::value
+  >::value;
+};
+
+template<unsigned N, unsigned ...args>
+struct Table<N, N, std::integer_sequence<unsigned, args...>> {
+  using value = typename std::conditional<
+      IsPrime<N, std::integer_sequence<unsigned, args...>>::value::value,
+      std::integer_sequence<unsigned, N>,
+      std::integer_sequence<unsigned>
+    >::type;
 };
 
 template<unsigned N>
 struct PrimeTable {
-  using value = typename AppendPrime<N, typename PrimeTable<N-1>::value>::value;
+  using value = typename ConcatPrime<N, typename PrimeTable<N-1>::value>::value;
 };
 
 template<>
--- Test/test_prime.cpp
+++ Test/test_prime.cpp
@@ -4,6 +4,13 @@
 
 #include <vector>
 
+template<unsigned ...args>
+void PrintSeq(std::integer_sequence<unsigned, args...> seq) {
+  ((std::cout << args << " "), ...);
+  std::cout << std::endl;
+}
+
+
 class PrimeTest : public ::testing::Test {
 protected:
     virtual void SetUp() {}
@@ -22,18 +29,46 @@ void PrimeTest::TearDownTestCase() {
 }
 
 TEST_F(PrimeTest, IsPrimeTest) {
-  std::vector<bool> tKase = {
-    IsPrime<5, 3, 2>::value::value,
-    IsPrime<7, 5, 3, 2>::value::value,
-  };
-  std::vector<bool> fKase = {
-    IsPrime<4, 3, 2>::value::value,
-    IsPrime<6, 5, 3, 2>::value::value,
-  };
-  for(auto t : tKase) {
-    ASSERT_EQ(t, true);
-  }
-  for(auto f : fKase) {
-    ASSERT_EQ(f, false);
-  }
+  IsPrime<5, std::integer_sequence<unsigned, 3, 2>>::value t1;
+  ASSERT_EQ(t1, true);
+  IsPrime<7, std::integer_sequence<unsigned, 5, 3, 2>>::value t2;
+  ASSERT_EQ(t2, true);
+  IsPrime<4, std::integer_sequence<unsigned, 3, 2>>::value f1;
+  ASSERT_EQ(f1, false);
+  IsPrime<6, std::integer_sequence<unsigned, 5, 3, 2>>::value f2;
+  ASSERT_EQ(f2, false);
+  IsPrime<8, std::integer_sequence<unsigned, 7, 5, 3, 2>>::value f3;
+  ASSERT_EQ(f3, false);
+}
+
+TEST_F(PrimeTest, AppendPrimeTest) {
+  Concat<std::integer_sequence<unsigned>, std::integer_sequence<unsigned>>::value t;
+  ASSERT_EQ(0, t.size());
+
+  std::is_same<
+    Concat<
+      std::integer_sequence<unsigned, 1, 2>,
+      std::integer_sequence<unsigned>
+    >::value,
+      std::integer_sequence<unsigned, 1, 2>
+    > t1;
+  ASSERT_EQ(t1, true);
+
+  std::is_same<
+    Concat<
+      std::integer_sequence<unsigned>,
+      std::integer_sequence<unsigned, 3, 4>
+    >::value,
+      std::integer_sequence<unsigned, 3, 4>
+    > t2;
+  ASSERT_EQ(t2, true);
+
+  std::is_same<
+    Concat<
+      std::integer_sequence<unsigned, 1, 2>,
+      std::integer_sequence<unsigned, 3, 4>
+    >::value,
+      std::integer_sequence<unsigned, 1, 2, 3, 4>
+    > t3;
+  ASSERT_EQ(t3, true);
 }
